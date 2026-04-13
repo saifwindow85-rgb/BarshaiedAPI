4b26f3a6d6ba7d7ff27c0a418ae024aa127bd2af
saifwindow85-rgb <saifwindow85@gmail.com>
2026-04-13 12:55:12 +0300

    Category Service&Repo Added To DI

--- BarshaiedAPI/Program.cs
+++ BarshaiedAPI/Program.cs
@@ -1,4 +1,6 @@
+using BusinessLayer.Services;
 using DataAccessLayer.AppDbContext;
+using DataAccessLayer.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BarshaiedDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<CategoryService>();
+builder.Services.AddScoped<CategoryRespository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
