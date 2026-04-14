6ffd4d00eac31bfd87c8829d654913cd936b0566
saifwindow85-rgb <saifwindow85@gmail.com>
2026-04-14 17:02:19 +0300

    ProductValidator Added

---
+++ BusinessLayer/DTOs/ProductDTOs/AddUpdateProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTOs.ProductDTOs
+{
+    public class AddUpdateProductDTO
+    {
+
+        public string ProductName { get; set; } = null!;
+        public string? Barcode { get; set; }
+        public int CategoryId { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public int Quantity { get; set; }
+        public int MinQuantity { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+
+    }
+}
---
+++ BusinessLayer/Validators/ProductValidator.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.DTOs.ProductDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validators
+{
+    public class ProductValidator : AbstractValidator<AddUpdateProductDTO>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName)
+           .Transform(name => name.Trim())
+           .NotEmpty().WithMessage("Product name is required")
+           .MinimumLength(3).WithMessage("Minimum length is 3")
+           .MaximumLength(100).WithMessage("Maximum length is 100");
+
+            // Barcode (اختياري)
+            RuleFor(x => x.Barcode)
+                .Length(8, 20)
+                .When(x => !string.IsNullOrEmpty(x.Barcode))
+                .WithMessage("Barcode must be between 8 and 20 characters")
+                .Matches(@"^\d+$")
+                .When(x => !string.IsNullOrEmpty(x.Barcode))
+                .WithMessage("Barcode must contain only digits");
+
+            // CategoryId
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("CategoryId must be valid");
+
+            // CostPrice
+            RuleFor(x => x.CostPrice)
+                .GreaterThan(0).WithMessage("Cost price must be greater than 0");
+
+            // SellPrice
+            RuleFor(x => x.SellPrice)
+                .GreaterThan(0).WithMessage("Sell price must be greater than 0")
+                .GreaterThanOrEqualTo(x => x.CostPrice)
+                .WithMessage("Sell price must be >= cost price");
+
+            // Quantity
+            RuleFor(x => x.Quantity)
+                .GreaterThanOrEqualTo(0).WithMessage("Quantity cannot be negative");
+
+            // MinQuantity
+            RuleFor(x => x.MinQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("MinQuantity cannot be negative")
+                .LessThanOrEqualTo(x => x.Quantity)
+                .WithMessage("MinQuantity cannot exceed Quantity");
+
+            // ExpiryDate
+            RuleFor(x => x.ExpiryDate)
+                .GreaterThan(DateTime.UtcNow)
+                .When(x => x.ExpiryDate.HasValue)
+                .WithMessage("Expiry date must be in the future");
+        }
+    }
+}
