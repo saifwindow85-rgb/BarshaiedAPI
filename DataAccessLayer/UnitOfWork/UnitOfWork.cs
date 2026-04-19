69825c85dd2cbbe8ab98d7ad69cb9143f0bbcbd5
saifwindow85-rgb <saifwindow85@gmail.com>
2026-04-19 21:57:11 +0300

    Improvments(IUnitOfWork&UnitOfWork)Added

---
+++ DataAccessLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Domain.AppDbContext;
+using Domain.Interfaces;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BarshaiedDbContext _context;
+        public ICategoryRepository categories { get; private set; }
+
+        public IProductRepository products { get; private set; }
+
+
+        public UnitOfWork(BarshaiedDbContext context)
+        {
+            _context = context;
+            categories = new CategoryRespository(_context);
+            products = new ProductRepository(_context);
+
+        }
+
+        public async Task<int> ComleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository categories { get; }
+        IProductRepository products { get; }
+
+        Task<int> ComleteAsync();
+    }
+}
